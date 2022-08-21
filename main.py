d81ef9442010976fbe7f25d045df95fbb3245a14
Alan Jaison <aj00643@gitlab.surrey.ac.uk>
2022-08-21 01:10:50 +0100

    can read file

---
+++ main.py
@@ -0,0 +1,13 @@
+# Auction Code by Alan Jaison for ThoughtMachine
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Auction Code by Alan Jaison for, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    f = open("input.txt", "r")
+
+    print(f.read())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('ThoughtMachine')
